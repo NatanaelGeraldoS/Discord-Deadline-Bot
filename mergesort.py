284c7ada2963c30b5fd163f4a12c678901e13c14
NatanaelGeraldoS <geradonatanael84@gmail.com>
2021-10-12 21:59:22 +0700

    ADD Refresh command

--- mergesort.py
+++ mergesort.py
@@ -1,24 +1,35 @@
 import datetime
 
+
+def convertdate(data):
+    # print("Asdad")
+    # print(data.year)
+    # print(data.month)
+    # print(data.day)
+    # print(datetime.date(data.year, data.month, data.day))
+    return datetime.date(data.year, data.month, data.day)
 # AI Tugas 2 (Aplha Beta Pruning) - 11/10/2021
+
+
 def takedate(data):
     date = data[data.index(' - ')+3:]
-    print(date)
+    # print(date)
     day = date[:2]
-    print(day)
+    # print(day)
     month = date[3:5]
-    print(month)
+    # print(month)
     year = date[6:]
-    print(year)
-    print(datetime.datetime(int(year), int(month), int(day), 0, 0, 0))
-    return datetime.datetime(int(year), int(month), int(day), 0, 0, 0) 
-    
+    # print(year)
+    # print(datetime.date(int(year), int(month), int(day)))
+    return datetime.date(int(year), int(month), int(day))
+
+
 def bubblesort(list):
 
-# Swap the elements to arrange in order
-   for iter_num in range(len(list)-1,0,-1):
-      for idx in range(iter_num):
-         if takedate(list[idx])>takedate(list[idx+1]):
-            temp = list[idx]
-            list[idx] = list[idx+1]
-            list[idx+1] = temp
+    # Swap the elements to arrange in order
+    for iter_num in range(len(list)-1, 0, -1):
+        for idx in range(iter_num):
+            if takedate(list[idx]) > takedate(list[idx+1]):
+                temp = list[idx]
+                list[idx] = list[idx+1]
+                list[idx+1] = temp
